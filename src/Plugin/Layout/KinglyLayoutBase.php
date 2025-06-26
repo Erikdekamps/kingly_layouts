44f655dbb04ce5fdddc6e29e4d1495e6d65c4427
Erik de Kamps <e.d.kamps@purmerend.nl>
2025-06-26 14:52:15 +0200

    feat: code cleanup and add caching

--- src/Plugin/Layout/KinglyLayoutBase.php
+++ src/Plugin/Layout/KinglyLayoutBase.php
@@ -2,11 +2,13 @@
 
 namespace Drupal\kingly_layouts\Plugin\Layout;
 
+use Drupal\Core\Cache\CacheBackendInterface;
 use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\Core\Layout\LayoutDefault;
 use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
 use Drupal\Core\Plugin\PluginFormInterface;
+use Drupal\taxonomy\TermInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
@@ -15,9 +17,24 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
 abstract class KinglyLayoutBase extends LayoutDefault implements PluginFormInterface, ContainerFactoryPluginInterface {
 
   /**
+   * The key used for the "None" option in select lists.
+   */
+  protected const NONE_OPTION_KEY = '_none';
+
+  /**
+   * The ID of the taxonomy vocabulary used for CSS colors.
+   */
+  protected const KINGLY_CSS_COLOR_VOCABULARY = 'kingly_css_color';
+
+  /**
+   * The field name on the taxonomy term that stores the hex color value.
+   */
+  protected const KINGLY_CSS_COLOR_FIELD = 'field_kingly_css_color';
+
+  /**
    * The entity type manager.
    *
-   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   * @var \Drupal\Core\Entity\EntityTypeInterface
    */
   protected $entityTypeManager;
 
@@ -27,6 +44,13 @@ abstract class KinglyLayoutBase extends LayoutDefault implements PluginFormInter
    * @var \Drupal\taxonomy\TermStorageInterface
    */
   protected $termStorage;
+
+  /**
+   * The cache backend.
+   *
+   * @var \Drupal\Core\Cache\CacheBackendInterface
+   */
+  protected $cache;
 
   /**
    * Constructs a new KinglyLayoutBase object.
@@ -39,11 +63,14 @@ abstract class KinglyLayoutBase extends LayoutDefault implements PluginFormInter
    *   The plugin implementation definition.
    * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
    *   The entity type manager.
+   * @param \Drupal\Core\Cache\CacheBackendInterface $cache_backend
+   *   The cache backend.
    */
-  public function __construct(array $configuration, $plugin_id, $plugin_definition, EntityTypeManagerInterface $entity_type_manager) {
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, EntityTypeManagerInterface $entity_type_manager, CacheBackendInterface $cache_backend) {
     parent::__construct($configuration, $plugin_id, $plugin_definition);
     $this->entityTypeManager = $entity_type_manager;
     $this->termStorage = $this->entityTypeManager->getStorage('taxonomy_term');
+    $this->cache = $cache_backend;
   }
 
   /**
@@ -54,7 +81,8 @@ abstract class KinglyLayoutBase extends LayoutDefault implements PluginFormInter
       $configuration,
       $plugin_id,
       $plugin_definition,
-      $container->get('entity_type.manager')
+      $container->get('entity_type.manager'),
+      $container->get('cache.default')
     );
   }
 
@@ -78,8 +106,8 @@ abstract class KinglyLayoutBase extends LayoutDefault implements PluginFormInter
     $configuration['gap_option'] = key($gap_options);
 
     // Default to no background or foreground color.
-    $configuration['background_color'] = '_none';
-    $configuration['foreground_color'] = '_none';
+    $configuration['background_color'] = self::NONE_OPTION_KEY;
+    $configuration['foreground_color'] = self::NONE_OPTION_KEY;
 
     // Add default for full width option.
     $configuration['full_width'] = FALSE;
@@ -104,7 +132,7 @@ abstract class KinglyLayoutBase extends LayoutDefault implements PluginFormInter
    */
   protected function getPaddingScaleOptions(): array {
     return [
-      '_none' => $this->t('None'),
+      self::NONE_OPTION_KEY => $this->t('None'),
       'xs' => $this->t('Extra Small (0.125rem)'),
       'sm' => $this->t('Small (0.25rem)'),
       'md' => $this->t('Medium (0.5rem)'),
@@ -179,14 +207,14 @@ abstract class KinglyLayoutBase extends LayoutDefault implements PluginFormInter
       ];
       $form['color_info'] = [
         '#type' => 'item',
-        '#markup' => $this->t('Colors are managed in the <a href="/admin/structure/taxonomy/manage/kingly_css_color/overview" target="_blank">Kingly CSS Color</a> vocabulary.'),
+        '#markup' => $this->t('Colors are managed in the <a href="/admin/structure/taxonomy/manage/@vocab_id/overview" target="_blank">Kingly CSS Color</a> vocabulary.', ['@vocab_id' => self::KINGLY_CSS_COLOR_VOCABULARY]),
       ];
     }
     else {
       $form['color_info'] = [
         '#type' => 'item',
         '#title' => $this->t('Color Options'),
-        '#markup' => $this->t('No colors defined. Please <a href="/admin/structure/taxonomy/manage/kingly_css_color/add" target="_blank">add terms</a> to the "Kingly CSS Color" vocabulary.'),
+        '#markup' => $this->t('No colors defined. Please <a href="/admin/structure/taxonomy/manage/@vocab_id/add" target="_blank">add terms</a> to the "Kingly CSS Color" vocabulary.', ['@vocab_id' => self::KINGLY_CSS_COLOR_VOCABULARY]),
       ];
     }
 
@@ -248,15 +276,27 @@ abstract class KinglyLayoutBase extends LayoutDefault implements PluginFormInter
    *   An associative array of color options.
    */
   protected function getColorOptions(): array {
-    $options = ['_none' => $this->t('None')];
+    $cid = 'kingly_layouts:color_options';
+    // New: Try to load from cache.
+    if ($cache = $this->cache->get($cid)) {
+      return $cache->data;
+    }
+
+    $options = [self::NONE_OPTION_KEY => $this->t('None')];
     if (!$this->entityTypeManager->getStorage('taxonomy_vocabulary')
-      ->load('kingly_css_color')) {
+      ->load(self::KINGLY_CSS_COLOR_VOCABULARY)) {
+      // New: Cache empty options if vocabulary doesn't exist.
+      $this->cache->set($cid, $options, CacheBackendInterface::CACHE_PERMANENT, ['taxonomy_term_list']);
       return $options;
     }
-    $terms = $this->termStorage->loadTree('kingly_css_color', 0, NULL, TRUE);
+    $terms = $this->termStorage->loadTree(self::KINGLY_CSS_COLOR_VOCABULARY, 0, NULL, TRUE);
     foreach ($terms as $term) {
       $options[$term->id()] = $term->getName();
     }
+
+    // New: Store in cache with a cache tag for invalidation.
+    $this->cache->set($cid, $options, CacheBackendInterface::CACHE_PERMANENT, ['taxonomy_term_list']);
+
     return $options;
   }
 
@@ -271,8 +311,9 @@ abstract class KinglyLayoutBase extends LayoutDefault implements PluginFormInter
 
     // Ensure that only one of "Edge to Edge" or "Full Width" is selected.
     if ($edge_to_edge && $full_width) {
-      $form_state->setErrorByName('edge_to_edge', $this->t('You cannot select both "Edge to Edge" and "Full Width (Background Only)". Please choose only one or neither.'));
-      $form_state->setErrorByName('full_width', $this->t('You cannot select both "Edge to Edge" and "Full Width (Background Only)". Please choose only one or neither.'));
+      $message = $this->t('You cannot select both "Edge to Edge" and "Full Width (Background Only)". Please choose only one or neither.');
+      $form_state->setErrorByName('edge_to_edge', $message);
+      $form_state->setErrorByName('full_width', $message);
     }
   }
 
@@ -302,68 +343,86 @@ abstract class KinglyLayoutBase extends LayoutDefault implements PluginFormInter
     $plugin_definition = $this->getPluginDefinition();
     $layout_id = $plugin_definition->id();
 
+    // Add layout-specific sizing class.
     if (!empty($this->configuration['sizing_option'])) {
       $build['#attributes']['class'][] = 'layout--' . $layout_id . '--' . $this->configuration['sizing_option'];
     }
 
-    // Start with the user-selected horizontal padding.
+    // Determine effective horizontal padding and apply full width/edge-to-edge
+    // classes.
     $h_padding_effective = $this->configuration['horizontal_padding_option'];
-
-    // Apply full width or edge-to-edge class if enabled.
     if (!empty($this->configuration['edge_to_edge'])) {
-      // 'Edge to Edge' uses custom padding from the viewport edge.
-      // The `kingly-layout-padding-x-*` class will be added below.
       $build['#attributes']['class'][] = 'kingly-layout--edge-to-edge';
+      // Edge to Edge uses custom padding from the viewport edge, so the utility
+      // class for horizontal padding will be applied below based on
+      // $h_padding_effective.
     }
     elseif (!empty($this->configuration['full_width'])) {
-      // 'Full Width' uses its own `calc()` padding to align with the site grid.
-      // We must prevent the padding utility class from being added.
       $build['#attributes']['class'][] = 'kingly-layout--full-width';
-      $h_padding_effective = '_none';
+      // Full Width uses its own `calc()` padding to align with the site grid,
+      // so we prevent the padding utility class from being added.
+      $h_padding_effective = self::NONE_OPTION_KEY;
     }
 
-    // Apply horizontal padding class if it's not been disabled.
-    if (!empty($h_padding_effective) && $h_padding_effective !== '_none') {
+    // Apply horizontal padding class.
+    if (!empty($h_padding_effective) && $h_padding_effective !== self::NONE_OPTION_KEY) {
       $build['#attributes']['class'][] = 'kingly-layout-padding-x-' . $h_padding_effective;
     }
 
     // Apply vertical padding class.
     $v_padding = $this->configuration['vertical_padding_option'];
-    if (!empty($v_padding) && $v_padding !== '_none') {
+    if (!empty($v_padding) && $v_padding !== self::NONE_OPTION_KEY) {
       $build['#attributes']['class'][] = 'kingly-layout-padding-y-' . $v_padding;
     }
 
     // Apply gap class.
     $gap = $this->configuration['gap_option'];
-    if (!empty($gap) && $gap !== '_none') {
+    if (!empty($gap) && $gap !== self::NONE_OPTION_KEY) {
       $build['#attributes']['class'][] = 'kingly-layout-gap-' . $gap;
     }
 
     // Apply background color.
-    $background_tid = $this->configuration['background_color'];
-    if (!empty($background_tid) && $background_tid !== '_none') {
-      /** @var \Drupal\taxonomy\TermInterface $term */
-      $term = $this->termStorage->load($background_tid);
-      if ($term && $term->bundle() === 'kingly_css_color' && $term->hasField('field_kingly_css_color') && !$term->get('field_kingly_css_color')
-        ->isEmpty()) {
-        $hex_color = $term->get('field_kingly_css_color')->value;
-        $build['#attributes']['style'][] = 'background-color: ' . $hex_color . ';';
-      }
+    $background_color_hex = $this->getTermColorHex($this->configuration['background_color']);
+    if ($background_color_hex) {
+      $build['#attributes']['style'][] = 'background-color: ' . $background_color_hex . ';';
     }
 
     // Apply foreground color.
-    $foreground_tid = $this->configuration['foreground_color'];
-    if (!empty($foreground_tid) && $foreground_tid !== '_none') {
-      /** @var \Drupal\taxonomy\TermInterface $term */
-      $term = $this->termStorage->load($foreground_tid);
-      if ($term && $term->bundle() === 'kingly_css_color' && $term->hasField('field_kingly_css_color') && !$term->get('field_kingly_css_color')
-        ->isEmpty()) {
-        $hex_color = $term->get('field_kingly_css_color')->value;
-        $build['#attributes']['style'][] = 'color: ' . $hex_color . ';';
-      }
+    $foreground_color_hex = $this->getTermColorHex($this->configuration['foreground_color']);
+    if ($foreground_color_hex) {
+      $build['#attributes']['style'][] = 'color: ' . $foreground_color_hex . ';';
     }
 
     return $build;
+  }
+
+  /**
+   * Retrieves the hex color value from a Kingly CSS Color taxonomy term.
+   *
+   * @param string $term_id
+   *   The ID of the taxonomy term.
+   *
+   * @return string|null
+   *   The hex color string if found and valid, NULL otherwise.
+   */
+  protected function getTermColorHex(string $term_id): ?string {
+    if (empty($term_id) || $term_id === self::NONE_OPTION_KEY) {
+      return NULL;
+    }
+
+    /** @var \Drupal\taxonomy\TermInterface $term */
+    $term = $this->termStorage->load($term_id);
+
+    // Ensure the term exists, is of the correct bundle, has the color field,
+    // and the field is not empty.
+    if ($term instanceof TermInterface &&
+      $term->bundle() === self::KINGLY_CSS_COLOR_VOCABULARY &&
+      $term->hasField(self::KINGLY_CSS_COLOR_FIELD) &&
+      !$term->get(self::KINGLY_CSS_COLOR_FIELD)->isEmpty()) {
+      return $term->get(self::KINGLY_CSS_COLOR_FIELD)->value;
+    }
+
+    return NULL;
   }
 
 }
